f0ec7a643839dce8d6647169d8ef310cabe77359
Ted <tchbell@gmail.com>
2017-06-20 15:49:56 +0000

    Fixed the client"

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,11 +4,16 @@ class Kele
     include HTTParty
     
     def initialize(email, password)
-        @email = email
-        @password = password
         @api_url = 'https://www.bloc.io/api/v1/sessions'
-        values = {"email": @email, "password": @password}
-        @user_auth_token = self.class.post(@api_url, values)
-        
+        values = {"email": email, "password": password}
+        response = self.class.post(@api_url, body: values)
+        @auth_token = response["auth_token"]
     end
+    
+#      def initialize(email, password)
+#     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+#     raise "invalid email/pass" if response.code != 200
+#     @auth_token = response["auth_token"]
+#   end
+    
 end
